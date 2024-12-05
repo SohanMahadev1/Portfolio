fb13c1955e1c45e6dc1f11fcb24d81a925d4ca07
Sohan Mahadev <smahadev1@hawk.iit.edu>
2024-12-05 12:58:05 -0600

    delete

--- src/components/animations/AnimationLottie.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Lottie from "lottie-react";
-
-const AnimationLottie = ({ animationData, className }) => {
-  return <Lottie animationData={animationData} className={className} />;
-};
-
-export default AnimationLottie;
